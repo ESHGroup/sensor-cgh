46db30ad48f0c4216d9c5da1597f635399ddd64c
ESHGroup <gil.gallegos@gmail.com>
2013-06-16 19:54:53 -0500

    Update HoloFilm.cpp
    Added License Copywrite

--- src/HoloFilm.cpp
+++ src/HoloFilm.cpp
@@ -1,8 +1,26 @@
-/*
- * HoloFilm.cpp
+/* Copyright (C) 2013 ESHGroup
  *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ *  HoloFilm.cpp
  *  Created on: May 27, 2013
- *  Last Modified: Jun 13, 2013
+ *  Last Modified: Jun 16, 2013
  *  Author: Richard McIntosh
  *  Email: rmcintosh@nmhu.edu
  */
@@ -86,9 +104,9 @@ complex<XnFloat> ** HoloFilm::GetInterferencePattern(XnPoint3D * obj_pts, int ob
 		}
 	}
 
-#ifdef _ESH_DEBUG
-	cout << "[DEBUG]: generated interference pattern [OK]" << endl;
-#endif
+
+	clog << "[DEBUG]: generated interference pattern [OK]" << endl;
+
 
 	return m_Points;
 }
