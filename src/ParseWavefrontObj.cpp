57a7362b937aa23a1bf43e5b1707c8b045d380e7
ESHGroup <gil.gallegos@gmail.com>
2013-06-16 20:04:24 -0500

    Update ParseWavefrontObj.cpp
    Added License Copywrite

--- src/ParseWavefrontObj.cpp
+++ src/ParseWavefrontObj.cpp
@@ -1,10 +1,30 @@
-/*
- * ParseWavefrontObj.cpp
- *
- *  Created on: Jun 7, 2013
- *  Last Modified Jun 13, 2013
- *  Author: Richard McIntosh
- *  Email: rmcintosh@nmhu.edu
+/* Copyright (C) 2013 ESHGroup
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+
+* ParseWavefrontObj.cpp
+*
+* Created on: Jun 7, 2013
+* Last Modified Jun 16, 2013
+* Author: Richard McIntosh
+* Email: rmcintosh@nmhu.edu
+*
  */
 
 #define __DEBUG
@@ -43,7 +63,7 @@ list<XnPoint3D> * WaveFrontObjFile::Parse(string FilePath) {
 
 		getline(f_stream, line);
 
-		cout << line << endl;
+		clog << line << endl;
 
 		istringstream str_stream(line);
 
@@ -69,7 +89,7 @@ list<XnPoint3D> * WaveFrontObjFile::Parse(string FilePath) {
 
 		XnPoint3D pt = (XnPoint3D)*itr;
 
-		cout << "x = " <<  pt.X << " y = " << pt.Y << " z = " << pt.Z << endl;
+		clog << "x = " <<  pt.X << " y = " << pt.Y << " z = " << pt.Z << endl;
 
 	}
 
